04221c29264fa2aba0544028c8a1679bd73a9c5a
Steven <stevenjoelbrey@gmail.com>
2016-01-20 17:46:02 -0700

    robust and tested function that can convert netcdf data to GIS data.

--- R/createPolygonFromNetCDF.R
+++ R/createPolygonFromNetCDF.R
@@ -15,18 +15,22 @@ library(R.utils)
 library(maps)
 library(ncdf4)
 
+################################################################################
+#  ARUGEMENTS
+################################################################################
+# ncFile     # Name of the netcdf file you want to open
+# latName    # name of the lat variable in netcdf file 
+# lonName    # name of the lon variable in netcdf file 
+# gridType   # "center" | "NW_edge". Which do the lat/lon describe of the grid? 
+# lonRange   # Array with c(west boundry, east boundry) of supplied nc data 
+# latRange   # Array with c(south boundry, north boundry) of supplied nc data 
+# variable   # the variable assigned to each grid box. Cannot be array. 
+# average    # if time dimension is provided, take time average? TRUE | FALSE
+# sumDims    # Take the sum over the a dimension. TRUE | FALSE
+# dimensions # which dimensions to sum | mean over using apply(). 1:2 default. 
+# fileSaveName # Name of the file to be saved 
+################################################################################
 
-# ARUGEMENTS
-ncFile  <- # Name of the netcdf file you want to open
-latName <- # name of the lat variable in netcdf file 
-lonName <- # name of the lon variable in netcdf file 
-gridType<- # "center" | "NW_edge". Which do the lat/lon describe of the grid? 
-variable<- # the variable assigned to each grid box. Cannot be array. 
-average <- # if time dimension is provided, take time average? TRUE | FALSE
-sumDims <- # Take the sum over the a dimension. TRUE | FALSE
-dimensions <- # which dimensions to sum | mean over using apply(). 1:2 default. 
-fileSaveName <- # Name of the file to be saved 
-    
 createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_regrid.nc",
                                     latName="lat",
                                     lonName="lon",
@@ -38,7 +42,6 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
                                     sumDims=TRUE,
                                     dimensions=1:2,
                                     fileSaveName="output.RData"
-                                    
                                     ){
   
   # Open the NEI netcdf file 
@@ -50,13 +53,14 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
   dx <- max(diff(lon))
   dy <- max(diff(lat))
   
+  # Show the user what the calculated dx and dy values are 
   warning(paste("dx =", dx, "and dy =", dy))
   
   # Get the chosen variable 
   if (average){
-    v <- apply(ncvar_get(nc, "NO"), dimensions, mean, na.rm=TRUE) 
+    v <- apply(ncvar_get(nc, variable), dimensions, mean, na.rm=TRUE) 
   } else if(sumDims){
-    v <- apply(ncvar_get(nc, "NO"), dimensions, sum, na.rm=TRUE) 
+    v <- apply(ncvar_get(nc, variable), dimensions, sum, na.rm=TRUE) 
   }
   
 
@@ -69,22 +73,23 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
   
   v <- v[lonMask, latMask]
   
-  # Create the polygons with attributes
-  
+  # Create the polygons with attributes (sp and spdf objects)
   polyCount <- 0 # Keep track of how many urban polygons we have
   polyList  <- list() # list to store polygon coordinate segments
   
   # Need to store value, create an array that is dim1 x dim2 long
+  # aka latxlon grid information
   uniquePoints <- length(lon) * length(lat)
   value        <- rep(NA, uniquePoints)
   
-  for (i in 1:length(lat)){
-    for (j in 1:length(lon)){
+  for (i in 1:length(lat)){   # loop through lat
+    for (j in 1:length(lon)){ # and for each lat go through each lon
      
       # Keep track of which polygon we are on 
       polyCount <- polyCount + 1 
       value[polyCount] <- v[j,i]
       
+      # Create corners based on what type of grid lat lon describe
       if(gridType=="center"){
       
         # East-West extent    
@@ -95,23 +100,21 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
         N <- lat[i] + dy/2 # top edge
         S <- N-dy          # bottom edge 
         
-      }
-      # TODO: Create corner
-      if(gridType=="NW_edge"){
+      } else if(gridType=="NW_edge"){
         stop("This edge grid functionality not developed yet")
       }
       
-      
       # Create coords of box starting in NW corner, going clockwise and 
-      # ending in the NEW corner
+      # ending in the NW corner
       plons <- c( W, E, E, W, W)
       plats <- c( N, N, S, S, N)
       
       coords <- cbind(plons, plats)
       
+      # Create a polygon object based on these corners
       Sr1 = Polygon(coords)
       
-      # Store this object in a list 
+      # Store this polygon object in a list 
       Srs1 = Polygons(list(Sr1), paste0("s",polyCount))
       
       polyList[[polyCount]] <- Srs1 
@@ -119,6 +122,9 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
       
     }
   }
+  
+  # If the number of datapoints (polyCount) is not equal to product of grid
+  # dimensions you have a problem! Throw a stop() and warning message 
   if(!polyCount == uniquePoints){
     stop("Something is wrong with grid logic. Data points and polys don't match")
   }
@@ -128,24 +134,71 @@ createPolygonFromNetCDF <- function(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_reg
   
   # Create the dataframe associated with these polygons
   df <- data.frame(value)
+  # Make the data.frame row names match the names of the polygons they are 
+  # associated with. This means adding an s to each name. 
   row.names(df) <- paste0("s",row.names(df))
+  
+  # Create the spatial polygon dataframe and return it to where the function is
+  # called 
   spdf <- SpatialPolygonsDataFrame(sp, data=df)
   return(spdf)
   
   
 }
-  
+
+################################################################################
+# Function testing ground for development. This data will be used for analysis
+# being done for Juliet. 
+################################################################################
+
+if(FALSE){
+# MAKE NOX
+
+# Get NO in US out of NEI
+NO <- createPolygonFromNetCDF(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_regrid.nc",
+                              latName="lat",
+                              lonName="lon",
+                              gridType="center",
+                              lonRange=c(-126,-60),
+                              latRange=c(15,51),
+                              variable="NO",
+                              average=FALSE,
+                              sumDims=TRUE,
+                              dimensions=1:2,
+                              fileSaveName="output.RData")
+
+NO2 <- createPolygonFromNetCDF(ncFile="EPAData/NEI08_2010_1x1_Jul_wkday_regrid.nc",
+                               latName="lat",
+                               lonName="lon",
+                               gridType="center",
+                               lonRange=c(-126,-60),
+                               latRange=c(15,51),
+                               variable="NO2",
+                               average=FALSE,
+                               sumDims=TRUE,
+                               dimensions=1:2,
+                               fileSaveName="output.RData")
+
+# Combine the NO + NO2 to make NOX. Note that you cannont add objects of class
+# spatialPolygonsDataframe
+NOXValues <- NO$value + NO2$value
+
+# copy the spatial component of NO2 for NOX
+NOX <- NO2
+
+# Give this the combined quantities of NO + NO2 
+NOX$value <- NOXValues 
+
+# Save this NOX quantitiy information
+save(NOX, file="EPAData/NEI_2008_wkdayNOX_spdf.RData")
+
+
+}
 
 
 
 
 
 
-# Save a figure showing urban polygons and top 10% of NOx emitters based on NEI
-pdf(file = "figures/NOXRegines.pdf",
-    width=10,
-    height=6)
-map("state")
-plot(Top10PercentNOX, add=TRUE, col="red")
 
-# Save the data 
+
